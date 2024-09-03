9da3c9420ce7f8635c8b30c4451830c74907fecf
xlebedx <xlebedx@gmail.com>
2024-09-03 19:22:59 +0300

    ready

--- module_3_1.py
+++ module_3_1.py
@@ -1,15 +1,19 @@
 calls = 0
 
 
-def count_calls(calls):
-    pass
+def count_calls():
+    global calls
+    calls += 1
+    return calls -1
 
 
 def string_info(string: str):
+    count_calls()
     return len(string), string.upper(), string.lower()
 
 
 def is_contains(string: str, list_to_search: list):
+    count_calls()
     lower_string = string.lower()
     lower_list = [element.lower() for element in list_to_search]
     if lower_string in lower_list:
@@ -19,6 +23,8 @@ def is_contains(string: str, list_to_search: list):
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(string_info('Capygeddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(is_contains('Suburban',['cycle','uRbaN','SuBuRbAn', ]))
 print(calls)
