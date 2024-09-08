8768351db87e4ed42645a5e024c5a30e93573781
xlebedx <xlebedx@gmail.com>
2024-09-08 15:21:37 +0300

    1

--- module_3_2/module_3_3.py
+++ module_3_2/module_3_3.py
@@ -6,3 +6,4 @@ values_list = ['string', 2, False]
 values_dict = {1: 2, 'строка': 'string2', True: False}
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
+print_params(*values_dict)
