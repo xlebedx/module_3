1917650490fb2d161732dd75007126f186cd4cb9
xlebedx <xlebedx@gmail.com>
2024-09-11 14:54:04 +0300

    ready?

--- module_3_2/module_3_3.py
+++ module_3_2/module_3_3.py
@@ -3,7 +3,11 @@ def print_params(a=1, b='строка', c=True):
 
 
 values_list = ['string', 2, False]
-values_dict = {1: 2, 'строка': 'string2', True: False}
+values_dict = {'a': 2, 'b': 'string2', 'c': False}
 values_list_2 = [54.32, 'Строка']
+print_params(*values_list)
+print_params(**values_dict)
 print_params(*values_list_2, 42)
-print_params(*values_dict)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
